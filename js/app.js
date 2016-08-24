05e514c5bd7635e077043d6532f9757b2c56d5c4
dana <dana@basken.com>
2016-08-24 19:52:10 -0400

    add 'show text' checkbox

--- js/app.js
+++ js/app.js
@@ -1,75 +1,81 @@
-angular.module("myapp", []).controller("HelloController", function($scope) {
+angular.module("JsonPrintApp", ['ngRoute'])
 
-  $scope.json = {};
-  $scope.json.parsedJson = '';
+  .controller("JsonPrintController", function($scope) {
 
-  $scope.extractJsonObjects = function(text) {
-    var results = [];
-    var braceCount = 0;
-    var candidate = '';
-    text.split('').forEach(function(character) {
-      switch (character) {
-        case '{':
-          if (braceCount == 0) {
-            results.push(candidate);
-            candidate = '';
-          }
-          braceCount++;
-          candidate += character;
-          break;
-        case '}':
-          braceCount--;
-          candidate += character;
-          if (braceCount == 0) {
-            candidate = candidate.split('\\"').join('"');
-            try {
-              candidate = JSON.stringify(JSON.parse(candidate), function(key, value) {
-                if (typeof value === 'string') {
-                  value = decodeURIComponent(value);
-                  var objects = this.extractJsonObjects(value);
-                  if (objects.length > 0) {
-                    return objects;
-                  }
+    $scope.json = {};
+    $scope.json.showSurroundingText = false;
+    $scope.json.parsedJson = '';
+  
+    $scope.extractJsonObjects = function(text) {
+      var results = [];
+      var braceCount = 0;
+      var candidate = '';
+      if (typeof text === 'string') {
+        text.split('').forEach(function(character) {
+          switch (character) {
+            case '{':
+              if (braceCount == 0) {
+                results.push(candidate);
+                candidate = '';
+              }
+              braceCount++;
+              candidate += character;
+              break;
+            case '}':
+              braceCount--;
+              candidate += character;
+              if (braceCount == 0) {
+                candidate = candidate.split('\\"').join('"');
+                try {
+                  candidate = JSON.stringify(JSON.parse(candidate), function(key, value) {
+                    if (typeof value === 'string') {
+                      value = decodeURIComponent(value);
+                      var objects = this.extractJsonObjects(value);
+                      if (objects.length > 0) {
+                        return objects;
+                      }
+                    }
+                    return value;
+                  }.bind(this), 3);
+    
+                  results.push(JSON.parse(candidate));
+                } catch(exception) {
+                  console.log(exception);
                 }
-                return value;
-              }.bind(this), 3);
-
-              results.push(JSON.parse(candidate));
-            } catch(exception) {
-              console.log(exception);
-            }
-            candidate = '';
+                candidate = '';
+              }
+              break;
+            default:
+              candidate += character;
+              break;
           }
-          break;
-        default:
-          candidate += character;
-          break;
+        }.bind(this));
       }
-    }.bind(this));
-    return results;
-  };
-
-  $scope.update = function(json) {
-    json.working = true;
-    clearTimeout($scope.sweatingTimeout);
-    $scope.sweatingTimeout = setTimeout(function() {
-      $scope.$apply(function () {
-        $scope.json.working = false;
-      });
-    }.bind(this), 500);
-
-    var objects = this.extractJsonObjects(json.sourceText);
-    json.parsedJson = '';
-
-console.log(objects);
-
-    objects.forEach(function(object) {
-      if (typeof object === 'string') {
-        json.parsedJson += object;
-      } else {
-        json.parsedJson += JSON.stringify(object, null, 3);
-      }
-    }.bind(this));
-  };
-
-});
+      return results;
+    };
+  
+    $scope.update = function(json) {
+      json.working = true;
+      
+      // fake timer just to show something's changed
+      clearTimeout($scope.sweatingTimeout);
+      $scope.sweatingTimeout = setTimeout(function() {
+        $scope.$apply(function () {
+          $scope.json.working = false;
+        });
+      }.bind(this), 500);
+  
+      var objects = this.extractJsonObjects(json.sourceText);
+      json.parsedJson = '';
+      objects.forEach(function(object) {
+        if (typeof object === 'string') {
+          if (json.showSurroundingText) {
+            json.parsedJson += object;
+          }
+        } else {
+          json.parsedJson += JSON.stringify(object, null, 3);
+        }
+      }.bind(this));
+    };
+  
+  });
